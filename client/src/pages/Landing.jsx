96dcbcbc61c461d4a57a27ca79d6ac329c9560f1
Divyaamohan <divyaadivyaa17@gmail.com>
2024-04-01 21:07:38 -0400

    Landing Page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,15 +1,36 @@
 import styled from 'styled-components';
-const StyledBtn = styled.button`
-  font-size: 1.5rem;
-  background: red;
-  color: white;
-`;
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Link } from 'react-router-dom';
+import main from '../assets/images/main.svg';
+import logo from '../assets/images/logo.svg';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>;<StyledBtn>styled btn</StyledBtn>
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span>app
+          </h1>
+          <p>
+            Lorem ipsum is placeholder text commonly used in the graphic, print,
+            and publishing industries for previewing layouts and visual mockups.
+          </p>
+
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn'>
+            Login/Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
   );
 };
+
 export default Landing;
