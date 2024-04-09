f90cea8059a6ac88759f7f7b1950f32a851a97ea
Divyaamohan <divyaadivyaa17@gmail.com>
2024-04-09 18:16:14 -0400

    Doyenv and morgan

--- client/server.js
+++ client/server.js
@@ -1,5 +1,13 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
 const app = express();
+import morgan from 'morgan';
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+dotenv.config();
+app.use(morgan('dev'));
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('hello world');
@@ -8,6 +16,8 @@ app.post('/', (req, res) => {
   console.log(req);
   res.json({ message: 'data received', data: req.body });
 });
-app.listen(5100, () => {
-  console.log('server running..');
+
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}..`);
 });
