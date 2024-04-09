99fcf52b90e65582e8cede1a2136c33c3a25b5f0
Divyaamohan <divyaadivyaa17@gmail.com>
2024-04-08 23:40:55 -0400

    server setup

---
+++ client/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+app.post('/', (req, res) => {
+  console.log(req);
+  res.json({ message: 'data received', data: req.body });
+});
+app.listen(5100, () => {
+  console.log('server running..');
+});
