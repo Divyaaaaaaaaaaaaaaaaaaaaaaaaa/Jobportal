f4041f1861016d5d14c41ab9aebb041667c3adcf
Divyaamohan <divyaadivyaa17@gmail.com>
2024-04-15 19:55:36 -0400

    async error

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -7,24 +7,18 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ company: 'apple' });
   res.status(200).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    res.status(400).json({ msg: `please provide company and position` });
-    return;
-  }
-  const id = nanoid(10);
-  // console.log(id);
-  const job = { id, company, position };
-  jobs.push(job);
+  const job = await Job.create(req.body);
   res.status(201).json({ job });
 };
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await Job.findById(id);
+
   if (!job) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
@@ -48,11 +42,10 @@ export const updateJob = async (req, res) => {
 };
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+  const removeJob = await Job.findByInAndDelete(id);
+  if (!removeJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  job = newJobs;
-  res.status(200).json({ msg: `job deleted` });
+
+  res.status(200).json({ msg: `job deleted`, job: removeJob });
 };
