b5ec67fdf74741c3f0dbfbd6eddc4ce142bd10ca
Divyaamohan <divyaadivyaa17@gmail.com>
2024-04-22 18:36:27 -0400

    User for register page

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import mongoose from 'mongoose';
 
 //router
 import jobRouter from './routes/jobRouter.js';
-
+import authRouter from './routes/authRouter.js';
 //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 if (process.env.NODE_ENV === 'development') {
@@ -23,6 +23,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('api/v1/jobs', jobRouter);
+app.use('api/v1/auth', authRouter);
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
 });
