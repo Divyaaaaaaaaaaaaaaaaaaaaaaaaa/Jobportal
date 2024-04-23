7d07c4c23bb91a64842d0fd0b3f68c3bd4805214
Divyaamohan <divyaadivyaa17@gmail.com>
2024-04-22 23:08:55 -0400

    login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { hashPassword } from '../utils/passwordUtils.js';
+import { UnauthenticatedError } from '../errors/customError.js';
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
@@ -11,5 +12,11 @@ export const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+
+  if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
   res.send('login');
 };
