dd7e59472de28e145cc9d1eb7b077c8dde669a6e
Divyaamohan <divyaadivyaa17@gmail.com>
2024-04-24 18:40:11 -0400

    authenticate user

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
 //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -22,7 +23,7 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.use('api/v1/jobs', jobRouter);
+app.use('api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
