16b7c5e0cbdb075bf8447e5ffddf12533aa465ba
Divyaamohan <divyaadivyaa17@gmail.com>
2024-05-03 13:52:09 -0400

    final Axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-fetch('/api/v1/test')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+import customFetch from './utils/customFetch.js';
+const resp = await customFetch.get('/test');
+console.log(resp);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
