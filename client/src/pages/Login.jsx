c42a4522d62caa094a8ff7acae262529b1abc0f1
Divyaamohan <divyaadivyaa17@gmail.com>
2024-05-08 13:50:02 -0400

    userActionData hook

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,10 @@
-import { Link, Form, redirect, useNavigation } from 'react-router-dom';
+import {
+  Link,
+  Form,
+  redirect,
+  useNavigation,
+  useActionData,
+} from 'react-router-dom';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 import { FormRow, Logo } from '../componenets';
 import customFetch from '../utils/customFetch';
@@ -6,24 +12,29 @@ import { toast } from 'react-toastify';
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const errors = { msg: '' };
+  if (data.password.length < 3) {
+    errors.msg = 'password too short';
+    return errors;
+  }
   try {
     await customFetch.post('/auth/login', data);
     toast.success('Login Successful');
     return redirect('/dashboard');
   } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
+    errors.msg = error?.response?.data?.msg;
+    return errors;
   }
-  
 };
 const Login = () => {
-  const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
+  const errors = useActionData();
   return (
     <Wrapper>
       <Form method='post' className='form'>
         <Logo />
         <h4>login</h4>
+        {errors?.msg && <p style={{ color: 'red' }}>{errors.msg}</p>}
+        <p></p>
         <FormRow type='email' name='email' defaultValue='divyaa@gmail.com' />
         <FormRow type='password' name='password' defaultValue='secret123' />
         <button type='submit' className='btn btn-block' disabled={isSubmitting}>
