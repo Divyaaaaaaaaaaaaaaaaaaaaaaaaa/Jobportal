86fa0e76adb626736a1f2f2dbe7ddbe4da57de8e
Divyaamohan <divyaadivyaa17@gmail.com>
2024-05-13 14:53:55 -0400

    job container

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,26 +1,30 @@
 import { toast } from 'react-toastify';
-import { JobsContainer, SearchContainer } from '../components';
+import { JobsContainer, SearchContainer } from '../componenets';
 import customFetch from '../utils/customFetch';
 import { useLoaderData } from 'react-router-dom';
 import { useContext, createContext } from 'react';
 export const loader = async () => {
-  try{
-   const {data}=await customFetch.get('/jobs')
-   return {data}
+  try {
+    const { data } = await customFetch.get('/jobs');
+    return { data };
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
   }
-  catch(error){
-     toast.error(error?.response?.data?.msg)
-     return error
-  }
-  
 };
-
+const AllJobsContext = createContext();
 const AllJobs = () => {
-  const {data}=useLoaderData()
+  const { data } = useLoaderData();
+  console.log(data);
 
-  return <>
-  <SearchContainer/>
-  <JobsContainer/>
-  </>;
+  return (
+    <>
+      <AllJobsContext.Provider value={{ data }}>
+        <SearchContainer />
+        <JobsContainer />
+      </AllJobsContext.Provider>
+    </>
+  );
 };
+export const useAllJobsContext = () => useContext(AllJobsContext);
 export default AllJobs;
