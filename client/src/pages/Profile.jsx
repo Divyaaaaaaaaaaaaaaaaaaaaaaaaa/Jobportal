ff773b8939495ad84d0bf7256e4316d082ca1947
Divyaamohan <divyaadivyaa17@gmail.com>
2024-05-16 21:43:50 -0400

    Profile action

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,11 +4,29 @@ import { useOutletContext } from 'react-router-dom';
 import { useNavigation, Form } from 'react-router-dom';
 import customFetch from '../utils/customFetch';
 import { toast } from 'react-toastify';
+export const action = async (request) => {
+  const formData = await request.formData();
+  const file = formData.get('avatar');
+  if (file && file.size > 500000) {
+    toast.error('Image size too large');
+    return null;
+  }
+  try {
+    await customFetch.patch('/users/update-user', formData);
+    toast.success('profile updated successfully');
+    return redirect('/dashboard/all-jobs');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+  return null;
+};
 const Profile = () => {
   const { user } = useOutletContext() || {};
   const { name, lastName, email, location } = user || {};
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
+
   return (
     <Wrapper>
       <Form method='post' className='form' encType='multipart/form-data'>
@@ -17,7 +35,7 @@ const Profile = () => {
           <div className='form-row'>
             <label htmlFor='avatar' className='form-label'>
               Select an image file(max 0.5 MB)
-            </label> 
+            </label>
             <input
               type='file'
               id='avatar'
