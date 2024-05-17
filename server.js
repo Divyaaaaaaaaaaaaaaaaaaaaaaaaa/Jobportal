68db365225ac71c6bffcbd3a07c61867f0f51ce3
Divyaamohan <divyaadivyaa17@gmail.com>
2024-05-17 11:55:44 -0400

    Cloudinary

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ dotenv.config();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import cloudinary from 'cloudinary';
+
+
 //router
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
@@ -19,6 +22,12 @@ import path from 'path';
 //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname,'./public')))
 if (process.env.NODE_ENV === 'development') {
