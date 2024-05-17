d87f1a4cee6ee8c751ef496c111046c0f04e44dd
Divyaamohan <divyaadivyaa17@gmail.com>
2024-05-16 22:12:34 -0400

    Multer setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
-import customFetch from './utils/customFetch.js';
 import { ToastContainer } from 'react-toastify';
-const resp = await customFetch.get('/test');
-console.log(resp);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
     <ToastContainer position='top-center' />
-  </React.StrictMode>
+  </>
 );
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,13 +9,14 @@ export const getCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ userWithoutPassword });
 };
 export const getApplicationStats = async (req, res) => {
-  const users = await user.countDocuments();
+  const users = await User.countDocuments();
   const jobs = await Job.countDocuments();
   res.status(StatusCodes.OK).json({ users, jobs });
 };
 export const updateUser = async (req, res) => {
+
   const obj = { ...req.body };
   delete obj.password;
-  const updatedUser = await USer.findByIdAndUpdate(req.user.userId, obj);
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
   res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
