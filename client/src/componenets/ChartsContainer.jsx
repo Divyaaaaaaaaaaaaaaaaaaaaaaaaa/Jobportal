ed3c5e51bd1ce3d5c008cbad2f31de94ca468d4f
Divyaamohan <divyaadivyaa17@gmail.com>
2024-05-24 16:44:28 -0400

    bar chart Container

--- client/src/componenets/ChartsContainer.jsx
+++ client/src/componenets/ChartsContainer.jsx
@@ -1,6 +1,16 @@
-const ChartsContainer=()=>{
-    return(
-        <h1>ChartsContainer</h1>
-    )
-}
-export default ChartsContainer
+import { useState } from 'react';
+
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+const ChartsContainer = ({data}) => {
+const [barChart,SetBarChart]=useState(true)
+  return <Wrapper>
+    <h4>Monthly Applications</h4>
+    <button type='button' onClick={()=> SetBarChart(!barChart)}>
+        {barChart? 'Area chart':'Bar Chart'}
+    </button>
+    {barChart? <BarChart data={data}/>:<AreaChart data={data}/>}
+  </Wrapper>
+};
+export default ChartsContainer;
